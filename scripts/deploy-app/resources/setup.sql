b306c6c9de8c391563f1de345c9edf2a211d9244
Mathilda Holmström <mathilda.holmstrom@telia.com>
2022-11-06 17:45:50 +0100

    Changed setup.sql

--- scripts/deploy-app/resources/setup.sql
+++ scripts/deploy-app/resources/setup.sql
@@ -1,8 +1,4 @@
-create
-database <db-name> character set utf8 collate utf8_bin;
-create
-user '<user>'@'localhost' identified by '<password>';
-grant all privileges on
-<db-name>.* to '<user>'@'localhost';
-flush
-privileges;
+create database microblog character set utf8 collate utf8_bin;
+create user 'microblog'@'localhost' identified by 'TestPass';
+grant all privileges on microblog.* to 'microblog'@'localhost';
+flush privileges;
