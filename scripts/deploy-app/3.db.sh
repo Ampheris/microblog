55627d70f74d479251f46040918e7ab5fb9c6431
Mathilda Holmström <mathilda.holmstrom@telia.com>
2022-11-06 17:47:15 +0100

    Changed 3.db file

--- scripts/deploy-app/3.db.sh
+++ scripts/deploy-app/3.db.sh
@@ -1,6 +1,6 @@
-db_user="<db_user>"
-db_password="<db_password>"
-db_name="<db_name>"
+db_user="microblog"
+db_password="microblog"
+db_name="microblog"
 
 sql_code=$(cat scripts/deploy-app/resources/setup.sql | sed "s/<db-name>/$db_name/; s/<user>/$db_user/; s/<password>/$db_password/")
 echo "$sql_code" | sudo mysql -uroot -p"$db_password"
